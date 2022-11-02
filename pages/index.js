0ae7343947709d6dd3c5905c09483f536a294a90
kame-turtle <n.kame.uesato@gmail.com>
2022-11-02 21:58:04 +0900

    Add custom app component settings. Add layout component that include header & footer component.

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import Header from "components/header";
-import Hero from "components/hero";
-import Footer from "components/footer";
+import Hero from 'components/hero';
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <main>
-        <Hero />
-      </main>
-      <Footer />
+      <Hero />
     </>
   );
 }
