f69f4e555f2ebb58c69e8d060f97530b546d6fa3
kame-turtle <n.kame.uesato@gmail.com>
2022-11-03 23:44:48 +0900

    Add props'imageOn' for image toggle at hero.js.

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,9 @@
-export default function Hero({title, subtitle}) {
+export default function Hero({title, subtitle, imageOn}) {
   return (
     <div>
       <h1>{title}</h1>
       <p>{subtitle}</p>
+      {imageOn && <figure>[image]</figure>}
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ import Hero from 'components/hero';
 export default function Home() {
   return (
     <>
-      <Hero title="じんぐらーふぁーむ" subtitle="農を愉しみ、食を愉しむ"/>
+      <Hero
+        title="じんぐらーふぁーむ"
+        subtitle="農を愉しみ、食を愉しむ"
+        imageOn
+      />
     </>
   );
 }
