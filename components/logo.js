d493d84ed4b98bf5317b1da02024a569caa8c031
kame-turtle <n.kame.uesato@gmail.com>
2022-11-08 22:43:01 +0900

    Add logo.module.css.

--- components/logo.js
+++ components/logo.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import styles from 'styles/logo.module.css'
 
-export default function Logo() {
+export default function Logo({boxOn = false}) {
   return (
-    <Link href="/">
+    <Link href="/" className={boxOn ? styles.box : styles.basic}>
       JinGra Farm
     </Link>
-  )
+  );
 }
