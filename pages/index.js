f6967c3378ed889a7a461d5a01e204d367bef10f
kame-turtle <n.kame.uesato@gmail.com>
2022-11-15 23:29:14 +0900

    Add container component. Apply window max-width settings.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
+import Container from 'components/container';
 import Hero from 'components/hero';
 
 export default function Home() {
   return (
-    <>
+    <Container>
       <Hero
         title="じんぐらーふぁーむ"
         subtitle="農を愉しみ、食を愉しむ"
         imageOn
       />
-    </>
+    </Container>
   );
 }
