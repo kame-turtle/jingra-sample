0c393c1c29e2a6dab82124a03567d78906389ff3
kame-turtle <n.kame.uesato@gmail.com>
2022-11-16 18:25:06 +0900

    Add post-body.js to text-styling.

---
+++ components/post-body.js
@@ -0,0 +1,10 @@
+import styles from 'styles/post-body.module.css'
+
+export default function PostBody({children}) {
+  return (
+    <div className={styles.stack}>
+      {children}
+    </div>
+  )
+}
+
